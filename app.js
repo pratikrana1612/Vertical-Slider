854befb988104d5aaa5d0c45c551b2cabdf05b8c
Rana Pratik <ranapratik1612@gmail.com>
2022-11-11 22:33:51 +0530

    traversion is completed

--- app.js
+++ app.js
@@ -1,46 +1,72 @@
 const upBtn = document.querySelector('.up');
 const downBtn = document.querySelector('.down');
-// const textDivs = document.querySelectorAll('.text_div');
-let translateValue=0;
+const textDivs = document.querySelectorAll('.left_side .text_div');
+// let translateValueup=0;
+// let translateValueDown = 
+let count = 1;
+
+function intialPosition(value)
+{
+    for(let i=0;i<textDivs.length;i++)
+    {
+        if(textDivs[i].classList.contains('active_div'))
+        {
+            // return;
+        }
+        else{
+            textDivs[i].style.transform=`translateY(${value}%)`;
+        }
+    }
+}
 
 upBtn.addEventListener('click',()=>
 {
+    count++;
     const active=document.querySelector('.left_side .active_div');
     let nextActive;
-    translateValue=translateValue-100;
-    if(translateValue==-400)
+    // translateValueup=translateValueup-100;
+    if(count==5)
     {
-        translateValue=0;
+        // translateValueup=0;
+        count=1;
         nextActive=document.querySelector('.left_side .text_div');
-
+        active.classList.remove('active_div');
+        nextActive.classList.add('active_div');
+        intialPosition(-100);
     }
     else
     {
         nextActive = active.nextElementSibling;
+        nextActive.classList.add('active_div');
+        active.classList.remove('active_div');
+        active.style.transform=`translateY(${100}%)`;
     }
     // active.style.nextActive=`translateY(-${translateValue}%)`;
-    nextActive.style.transform=`translateY(${translateValue}%)`;
-    active.style.transform=`translateY(${100}%)`;
-    nextActive.classList.add('active_div');
-    active.classList.remove('active_div');
+    nextActive.style.transform=`translateY(${0}%)`;
 })
 
 downBtn.addEventListener('click',() =>
 {
+    count--;
     const active=document.querySelector('.left_side .active_div');
-    let prevActive;
-    translateValue=translateValue+100;
-    // if(translateValue==-400)
-    // {
-    //     translateValue=0;
-    //     prevActive=document.querySelector('.left_side .text_div');
-    // }
-    // else
-    prevActive = active.previousElementSibling;
+    let nextActive;
+    if(count==0)
+    {
+        count=4;
+        nextActive=document.querySelector('.left_side .text_div:last-of-type');
+        nextActive.classList.add('active_div');
+        active.classList.remove('active_div');
+        intialPosition(100);
+    }
+    else
+    {
+        nextActive= active.previousElementSibling;
+        nextActive.classList.add('active_div');
+        active.classList.remove('active_div');
+        active.style.transform=`translateY(${-100}%)`;
+    }
     // active.style.nextActive=`translateY(-${translateValue}%)`;
-    prevActive.style.transform=`translateY(${translateValue}%)`;
-    active.style.transform=`translateY(${-100}%)`;
-    prevActive.classList.add('active_div');
-    active.classList.remove('active_div');
+    nextActive.style.transform=`translateY(${0}%)`;
+
 
 })
